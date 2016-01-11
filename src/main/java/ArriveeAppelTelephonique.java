ebe9e235cab4c96e146e5a95da46f11a273f050d
Squalex <larry.motalavigne@gmail.com>
2016-01-11 18:17:49 +0100

    Oh aime Geai !!

--- src/main/java/ArriveeAppelTelephonique.java
+++ src/main/java/ArriveeAppelTelephonique.java
@@ -4,6 +4,10 @@
 public class ArriveeAppelTelephonique extends Event {
     @Override
     public void run() {
+        Entite newAppel = new Appel_Telephonique();
+
+        float t_at = Generateur.loi_exponentielle_appelTelephonique(Statistique.date_simu);
+        Timing.addNewEvenement(Statistique.date_simu + t_at, new ArriveeAppelTelephonique());
 
     }
 }
