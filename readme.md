350af4000bbca2092ee1ac95604006678302b16e
squalex <larry.motalavigne@gmail.com>
2017-08-29 22:04:36 +0200

    - Add Readme - Clean code

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Simulation d'un centre d'appel
+
+L'objectif de cet exercice est de simuler le processus réalisé en centre d'appel afin d'estimer le temps moyen de réponse.
+
+
+
+
+
+Il convient de préciser que cette simulation prends en compte une unique journée pour le traitement des courriers/appels.
+La journée commence à 8h et se termine à 17h.
+
+
+
+## Appels Téléphoniques
+L'arrivée des appels téléphoniques suit une loi exponentielle de paramètre 0.2
+
+
+## Courriels
+
+
+
+
+# À propos
+
+Cet exercice a été réalisé dans un cadre scolaire lors de la 5e année d'<a href="http://polytech.univ-tours.fr/">Ecole d'Ingénieur de Polytech Tours</a>.
+Ce sujet, proposé par <b>Christophe Lenté</b>, a permis d'aborder des notions de statistiques mais aussi de découvrir une autre facette de la programmation.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,16 @@ import Statistiques.Statistique;
 public class Main {
     public static void main(String[] args) {
         int nbIterations = 10000; // Nombre d'itérations
-        int Nc = 8; // Nombre de téléconseillers attribués aux courriels
-        int Na = 3; // Nombre de téléconseillers attribués aux appels
+        int nombreTeleconseillersCourriels = 8; // Nombre de téléconseillers attribués aux courriels
+        int nombreTeleconseillersAppels = 3; // Nombre de téléconseillers attribués aux appels
         // ATTENTION : NaMax doit être >= à Na
-        int NaMax = 11; // Quantité de postes disponibles pour les appels téléphoniques
+        int nombrePostesDisponibleAppels = 11; // Quantité de postes disponibles pour les appels téléphoniques
 
         Simulation.dateDebutSimu = 0;
         Simulation.date_simu = Simulation.dateDebutSimu;
 
         for (int i = 0; i < nbIterations; i++) {
-            Bureau.init(Nc,Na,NaMax);
+            Bureau.init(nombreTeleconseillersCourriels,nombreTeleconseillersAppels,nombrePostesDisponibleAppels);
 
             Event event = new Debut();
             event.run();
